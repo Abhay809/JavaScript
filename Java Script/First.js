a9331a86ce917663a587752f257de8eddbd118e3
Abhay Kumar Mittal <123982720+Abhay809@users.noreply.github.com>
2024-06-08 01:45:51 +0530

    Add files via upload

---
+++ Java Script/First.js
@@ -0,0 +1,36 @@
+console.log("Abhay Mittal") // console.log to print on console
+console.log("first day of learning js!!");
+
+// variables
+name = "Abhay Miital";
+age  = 49;
+price = 44.56;
+x = null;  // no value in x variable  // meaning:= we know that it has a empty value
+y = undefined;  // both null and undefined are different  // meaning:= we don't know what is inside the varible
+isFollow = true;  // boolean type value
+console.log(name);
+console.log(price);
+console.log(x);
+console.log(isFollow);
+
+// javascript is a dynamically typed language
+// we donot require to declare the type of a variable in it
+// On runtime it will automatically calculte which type of datatype variable is
+
+// variable rules
+fullname = "Abhay Mittal";
+fullName = "Shubham Mittal";
+console.log(fullname);
+console.log(fullName);
+
+// full name = "abhay mittal";  not allowed
+// fullname$,fullname_,fullname123 are allowed
+// $fullname, _fullname are allowed but 123fullname is not
+
+// reserverd words
+// console = "abhay mittal";
+// console.log(console);  give an error 
+Console = "abhay Mital";
+console.log(Console);
+
+// fullName - camelcase - we generally use
